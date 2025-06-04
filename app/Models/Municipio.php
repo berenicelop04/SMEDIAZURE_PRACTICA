9ffb366c55a5abb3c166fea25151a35bb483af09
G3R-V <gerardo.villalobos.balam@gmail.com>
2025-06-04 12:05:28 -0500

    Se agrego una función al modelo municipio para compartir datos de forma correcta con localidades

--- app/Models/Municipio.php
+++ app/Models/Municipio.php
@@ -13,4 +13,10 @@ class Municipio extends Model
     public $timestamps = false;
 
     protected $fillable = ['municipio'];
+
+    public function localidades()
+    {
+        return $this->hasMany(Localidad::class, 'id_municipio');
+    }
+
 }
